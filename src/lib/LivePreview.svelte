b47f0ae12cf63a6db51dcd1cd330b66aef4d6f98
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-03-05 12:34:40 +0000

    tweak

--- src/lib/LivePreview.svelte
+++ src/lib/LivePreview.svelte
@@ -6,19 +6,7 @@
 	
 $: item = data.posts.filter(x=>x.id==postId)[0]
 
-
-
-let css = `
-@import url("https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Playfair+Display:wght@500&display=swap");
-
-h2{
-	font-family: 'DM Serif Display', serif; font-weight: 600; font-size: 35px; margin-bottom: 25px; line-height: 1.2"
-}
-
-p{
-	font-size: 19px; line-height: 29px; font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-}
-`
+let css  = data.settings.preview_css;
 
 </script>
 
