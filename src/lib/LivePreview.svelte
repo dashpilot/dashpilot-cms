db9c8f3b03064fc87bb9927d968c41b0fe62724f
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-03-05 15:25:41 +0000

    The preview now also supports markdown

--- src/lib/LivePreview.svelte
+++ src/lib/LivePreview.svelte
@@ -1,23 +1,45 @@
 
 <script>
+	import SvelteMarkdown from 'svelte-markdown'
+	
 	export let data;
 	export let postId;
 	let item = data.posts.filter(x=>x.id==postId)[0]
+	let cat = data.categories.filter(x=>x.id==item.category)[0]
+	let type = data.types.filter(x=>x.slug==cat.type)[0]
+	let fields = type.fields;
 	
 $: item = data.posts.filter(x=>x.id==postId)[0]
 
 let css  = data.settings.preview_css;
 
+
+ 
+
 </script>
 
 <div class="preview">
+	
+
 
 <h2>{item.title}</h2>
 
+{#each fields as field}
+	{#if field.type=='rte'}
+		{@html item[field.title]}
+	{/if}
+	
+	{#if field.type=='mde'}
+		<SvelteMarkdown bind:source={item[field.title]} />
+	{/if}
+	
+	{#if field.type=='txta'}
+		{item[field.title]}
+	{/if}
 
-<div>
-{@html item.body}
-</div>
+{/each}
+
+
 
 {@html `<style>${css}</style>`}
 </div>
